bf91d1acac3a2dec3b9dc9be1852fb6a879312c4
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-07-30 13:35:13 +0200

    whitespaces and end

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-from mysql_quote_identifiers import quote_identifier
+import unittest
 
 
-def p_quote_identifier(i):
-    pad_char = ""
-    print(f"{i:{pad_char}<30} => `{quote_identifier(i)}`")
-
-
-if __name__ == "__main__":
-    p_quote_identifier("test test")
-    p_quote_identifier("test test \\")
-    p_quote_identifier("test`; SELECT * FROM user")
+class TestQuoting(unittest.TestCase):
+    def test_foo()
