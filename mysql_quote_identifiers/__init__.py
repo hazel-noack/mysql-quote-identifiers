a2f8f8912745bc8eb7b52c4462a715ecf065ecc3
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-07-30 12:47:16 +0200

    linked spec

--- mysql_quote_identifiers/__init__.py
+++ mysql_quote_identifiers/__init__.py
@@ -5,6 +5,7 @@ __name__ = "mysql_quote_identifiers"
 __folder__ = str(pathlib.Path(__file__).parent)
 
 
+# https://stackoverflow.com/questions/51867550/pymysql-escaping-identifiers
 # https://mariadb.com/docs/server/reference/sql-structure/sql-language-structure/identifier-names
 def quote_identifier(identifier: str) -> str:
     return identifier
