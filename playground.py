cd3461922fac22efb90c374b5bc29b880fdf8b47
Hazel Noack <hazel@webcontact.de>
2025-07-30 17:13:02 +0200

    updated default values

--- playground.py
+++ playground.py
@@ -1,7 +1,5 @@
-import random
 from mysql_quote_identifiers import escape_identifier
-from mysql_quote_identifiers.reserved_words import RESERVED_WORDS, RESERVED_WORDS_ORACLE_MODE
 
 
 if __name__ == "__main__":
-    print(random.choices(list(RESERVED_WORDS), k=10))
+    print(escape_identifier("foo-bar"))
