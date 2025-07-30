ffc0d544add85940f24ca472c0c034411fa32178
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-07-30 12:56:25 +0200

    basic escaping

--- mysql_quote_identifiers/__init__.py
+++ mysql_quote_identifiers/__init__.py
@@ -1,11 +1,17 @@
-import pathlib
-
-
 __name__ = "mysql_quote_identifiers"
-__folder__ = str(pathlib.Path(__file__).parent)
+
+
+MALICIOUS_CHARACTER = ["`", "\\"]
 
 
 # https://stackoverflow.com/questions/51867550/pymysql-escaping-identifiers
 # https://mariadb.com/docs/server/reference/sql-structure/sql-language-structure/identifier-names
 def quote_identifier(identifier: str) -> str:
-    return identifier
+    quoted = ""
+    for char in identifier:
+        if char in MALICIOUS_CHARACTER:
+            char = "\\" + char
+        
+        quoted += char
+
+    return quoted
--- test.py
+++ test.py
@@ -2,9 +2,11 @@ from mysql_quote_identifiers import quote_identifier
 
 
 def p_quote_identifier(i):
-    print(quote_identifier(i))
+    pad_char = ""
+    print(f"{i:{pad_char}<30} => `{quote_identifier(i)}`")
 
 
 if __name__ == "__main__":
-    print("testing script")
-    quote_identifier("test test")
+    p_quote_identifier("test test")
+    p_quote_identifier("test test \\")
+    p_quote_identifier("test`; SELECT * FROM user")
