297d46bade1d0551943a85fb32a007394e8e334c
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-07-30 13:44:01 +0200

    whitespace end test

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import unittest
+import logging
+from mysql_quote_identifiers import quote_identifier, IdentifierException, IdentifierType
 
 
-class TestQuoting(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual("foo", "bar")
+logger = logging.getLogger("mysql_quote_identifiers")
+
+
+class TestFurtherRules(unittest.TestCase):
+    """
+    Testing the further rules section of the spec
+    https://mariadb.com/docs/server/reference/sql-structure/sql-language-structure/identifier-names#further-rules
+    """
+    def test_whitespaces(self):
+        """
+        Database, table and column names can't end with space characters
+        """
+        with self.assertRaises(IdentifierException):
+            quote_identifier("foo ", IdentifierType.DATABASE)
